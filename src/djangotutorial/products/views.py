1b9dd0eb457153ec2e17b063ad475bac4bdca38c
baromeg <baromeg@gmail.com>
2021-04-13 09:48:08 +0100

    Raw html form

--- src/djangotutorial/products/views.py
+++ src/djangotutorial/products/views.py
@@ -7,14 +7,13 @@ from .models import Product
 
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
-    context = {
-        'form': form
-    }
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == 'POST':
+        my_new_title = request.POST.get('title')
+        # Product.objects.create(title=my_new-title)
+        print(my_new_title)
+    context = {}
     return render(request, 'products/product_create.html', context)
 
 

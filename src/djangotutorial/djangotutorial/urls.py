e36b96313598282861c31709141b510cc05e90b0
baromeg <baromeg@gmail.com>
2021-04-13 11:22:31 +0100

    Dynamic URL Routing

--- src/djangotutorial/djangotutorial/urls.py
+++ src/djangotutorial/djangotutorial/urls.py
@@ -17,16 +17,16 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, projects_view, about_view
-from products.views import product_detail_view, product_create_view, render_initial_data
+from products.views import product_detail_view, product_create_view, render_initial_data, dynamic_lookup_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
-    path('contact/', contact_view, name='home'),
-    path('create/', render_initial_data, name='home'),
-    path('product/', product_detail_view, name='home'),
-    path('projects/', projects_view, name='home'),
-    path('about/', about_view, name='home'),
+    path('contact/', contact_view, name='contact'),
+    path('create/', render_initial_data, name='create'),
+    path('product/<int:id>/', dynamic_lookup_view, name='product'),
+    path('projects/', projects_view, name='projects'),
+    path('about/', about_view, name='about'),
 
     path('admin/', admin.site.urls),
 ]
--- src/djangotutorial/products/views.py
+++ src/djangotutorial/products/views.py
@@ -6,6 +6,13 @@ from .models import Product
 # Create your views here.
 
 
+def dynamic_lookup_view(request, id):
+    obj = Product.objects.get(id=id)
+    context = {
+        'object': obj
+    }
+    return render(request, 'products/product_details.html', context)
+
 # def product_create_view(request):
 #     my_form = RawProductForm()
 #     if request.method == 'POST':
@@ -31,6 +38,7 @@ from .models import Product
 #         print(my_new_title)
 #     context = {}
 #     return render(request, 'products/product_create.html', context)
+
 
 def render_initial_data(request):
     initial_data = {

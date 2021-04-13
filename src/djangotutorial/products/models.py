69c2c54375f0d67e294c1d298b72283f5b5e791c
baromeg <baromeg@gmail.com>
2021-04-13 14:32:44 +0100

    Django URLs reverse

--- src/djangotutorial/products/models.py
+++ src/djangotutorial/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -10,6 +10,7 @@ class Product(models.Model):
     # blank=False === to required - # null=False === is required in the database
     summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False)  # null=True, default=True
+
     def get_absolute_url(self):
-        return f'/products/{self.id}'
-    
+        # f'/products/{self.id}/'
+        return reverse('product-detail', kwargs={'id': self.id})
